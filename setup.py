235f5687ac3e3a7d2b004af43d8b9821fe81431f
Tobia Carozzi <tobia@chalmers.se>
2018-05-22 13:43:37 +0200

    Corrected some errors.

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name = 'LiSA',
+setup(name = 'iLiSA',
       version = '2.0',
       description = 'Package for international LOFAR station in stand-alone mode',
       author = 'Tobia D. Carozzi',
       author_email = 'tobia.carozzi@chalmers.se',
       packages = find_packages(),
       package_data = {
-            'ilisa.observations': ['access_config.yml'],
-            'ilisa.antennameta': ['share/CalTables/*.dat',
+            'ilisa.observations': ['access_config_DIST.yml'],
+            'ilisa.antennameta': ['share/CalTables/*/data/*.dat',
                                   'share/StaticMetaData/*.conf']},
       license = 'ISC',
       classifiers = [
-          'Development Status :: 1 - Planning',
+          'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: ISC License',
           'Programming Language :: Python :: 2.7',
