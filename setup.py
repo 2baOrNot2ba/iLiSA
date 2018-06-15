486d01771439d8ffbc7c6c161a823d06e4bcdd28
Tobia Carozzi <tobia@chalmers.se>
2018-06-15 15:43:56 +0200

    Tidy up (PEP8).

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name = 'iLiSA',
-      version = '2.1',
-      description = 'Package for international LOFAR station in stand-alone mode',
-      author = 'Tobia D. Carozzi',
-      author_email = 'tobia.carozzi@chalmers.se',
-      packages = find_packages(),
-      package_data = {
+setup(name='iLiSA',
+      version='2.1',
+      description="""Package for handling international LOFAR station in
+                  stand-alone mode""",
+      author='Tobia D. Carozzi',
+      author_email='tobia.carozzi@chalmers.se',
+      packages=find_packages(),
+      package_data={
             'ilisa.observations': ['access_config_DIST.yml'],
             'ilisa.antennameta': ['share/CalTables/*/data/*.dat',
                                   'share/StaticMetaData/*.conf']},
-      license = 'ISC',
-      classifiers = [
+      license='ISC',
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: ISC License',
@@ -27,5 +28,5 @@ setup(name = 'iLiSA',
           'python-casacore',
           'matplotlib>=1.5'
       ],
-      scripts = ['scripts/']
-     )
+      scripts=['scripts/']
+      )
