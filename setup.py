418c6d10c3ca83807dd7ee0608d6f8e8d6689869
Tobia Carozzi <tobia@chalmers.se>
2019-12-02 12:08:35 +0100

    Increment to version 3.13

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(name='iLiSA',
           'PyYAML',
           'h5py'
       ],
-      scripts=['scripts/acc2bst.py', 'scripts/ilisa_adm.py', 'scripts/ilisa_rec.py',
+      scripts=['scripts/ilisa_adm.py', 'scripts/ilisa_rec.py',
                'scripts/ilisa_sched.py', 'scripts/ilisa_view.py']
       )
