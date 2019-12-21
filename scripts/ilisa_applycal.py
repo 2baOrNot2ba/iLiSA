17ce245e5a9cf3854fe129396aa1e7cc4d7eef40
Tobia Carozzi <tobia@chalmers.se>
2019-12-21 15:49:33 +0100

    Added script that applies a caltab file to a CVC folder. It is created in a copy of raw CVC folder.

---
+++ scripts/ilisa_applycal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""Apply a calibration file to ACC or XST data folder.
+"""
+
+import argparse
+import ilisa.observations.imaging as imaging
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cvcpath',
+                        help="""Path to CVC folder""")
+    parser.add_argument('caltabpath', help="""Path to caltab file""")
+    args = parser.parse_args()
+
+    imaging.cvcfolder_applycal(args.cvcpath, args.caltabpath)
