b21b46cac77d41fc873985a1ef4ab0a1462e7984
Tobia Carozzi <tobia@chalmers.se>
2020-01-24 11:39:05 +0100

    Refactor of export module: removed all output_all* funcs. Also added support NenuFAR. Improved plot_arrconfig. Added CLI options to do only one station.

--- ilisa/antennameta/export.py
+++ ilisa/antennameta/export.py
@@ -3,20 +3,19 @@
 other formats such as CASA .cfg files.
 """
 import os
-import sys
 import datetime
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from ilisa.antennameta.antennafieldlib import parseAntennaField, \
-                         parseiHBADeltasfile, getArrayBandParams, list_stations
+from ilisa.antennameta.antennafieldlib import parseAntennaField, parseiHBADeltasfile,\
+    getArrayBandParams, list_stations
 
 BANDARRS = ['LBA', 'HBA']
-CASA_CFG_DTYPE = [('X',float),('Y',float),('Z',float),('Diam',float),('Name','S5')]
+CASA_CFG_DTYPE = [('X', float), ('Y', float), ('Z', float), ('Diam', float),
+                  ('Name', 'S5')]
 CASA_CFG_FMT = '%12f %12f %12f %4.1f %s'
-CASA_CFG_DEST = os.path.join(os.path.dirname(__file__),'share/simmos/')
-ALIGNMENT_DEST = os.path.join(os.path.dirname(__file__),'share/alignment/')
+CASA_CFG_DEST = os.path.join(os.path.dirname(__file__), 'share/simmos/')
+ALIGNMENT_DEST = os.path.join(os.path.dirname(__file__), 'share/alignment/')
 
 
 def _get_casacfg_header(tier, bandarr=None, stnid=None):
@@ -55,7 +54,8 @@ def _get_casacfg_header(tier, bandarr=None, stnid=None):
 
 
 def output_arrcfg_station(stnid, bandarr, output='default'):
-    """Output a station array configuration in a CASA simmos .cfg format.
+    """Output a station array (given by stnid and bandarr) configuration in a CASA simmos
+    .cfg format.
     """
     stnPos, stnRot, stnRelPos, stnIntilePos = getArrayBandParams(stnid, bandarr)
     header = _get_casacfg_header('station', bandarr, stnid)
@@ -94,6 +94,13 @@ def output_arrcfg_tile(stnid):
 
 
 def output_rotmat_station(stnid, bandarr, output='default'):
+    """
+    Save a station bandarray's rotation matrix.
+    :param stnid: Station ID.
+    :param bandarr: 'HBA' or 'LBA'
+    :param output: Name of output file. If set to 'default', will use default name
+                   convention, i.e. '<stnid>_<bandarr>.txt'.
+    """
     stnpos, stnrot, stnrelpos, stnintilepos = \
                          getArrayBandParams(stnid, bandarr)
     if output=='default':
@@ -102,64 +109,7 @@ def output_rotmat_station(stnid, bandarr, output='default'):
     np.savetxt(output, stnrot, fmt="%12f %12f %12f", header=header)
 
 
-def output_all_rotmats():
-    """Save all LOFAR station rotation matrices. The rotation matrices define
-    the alignment of the stations."""
-    stnid_list = sorted(list_stations())
-    for stnid in stnid_list:
-        for bandarr in BANDARRS:
-            print("{} {}".format(stnid, bandarr))
-            output_rotmat_station(stnid, bandarr)
-
-
-def output_all_arrcfg_ILT():
-    """Save ILT's array configuration in a CASA sim .cfg format, for both LBA
-       and HBA.
-    """
-    stnId_list = list_stations()
-    stnId_list.sort(key=lambda stnid: stnid[2:])
-    nrstns = len(stnId_list)
-    the_maxbaselines = maxbaselinelen()
-    for bandarr in BANDARRS:
-        header = _get_casacfg_header('ILT', bandarr)
-#        if bandarr == 'LBA':
-#            diam = 55.5
-#        else:
-#            diam = 63.3
-        outtable = np.zeros(nrstns, dtype=CASA_CFG_DTYPE)
-        for stnnr, stnid in enumerate(stnId_list):
-            AntFld = parseAntennaField(stnid)
-            position = AntFld[bandarr]['POSITION']
-            outtable['X'][stnnr], outtable['Y'][stnnr], outtable['Z'][stnnr] = position
-            try:
-                outtable['Diam'][stnnr] = the_maxbaselines[stnid][bandarr]
-            except KeyError:
-                outtable['Diam'][stnnr] = the_maxbaselines[stnid]['HBA0']
-            outtable['Name'][stnnr] = stnid
-        filename = os.path.join(CASA_CFG_DEST, "ILT_"+bandarr+'.cfg')
-        np.savetxt(filename, outtable, fmt=CASA_CFG_FMT, header=header)
-
-
-def output_all_stations_arrcfg():
-    """Save all station array configurations.
-    """
-    stnId_list = list_stations()
-    for bandarr in BANDARRS:
-        for stnid in stnId_list:
-            print stnid, bandarr
-            output_arrcfg_station(stnid, bandarr)
-
-
-def output_all_tile_arrcfg():
-    """Save all tile array configurations.
-    """
-    stnId_list = list_stations()
-    bandarr = 'HBA'
-    for stnid in stnId_list:
-        output_arrcfg_tile(stnid)
-
-
-def maxbaselinelen():
+def max_stn_baselines():
     """Compute max baseline length of each station."""
     stnId_list = list_stations()
     the_maxbaselines = {}
@@ -185,26 +135,23 @@ def maxbaselinelen():
     return the_maxbaselines
 
 
-def plot_arrayconfiguration(stnid, bandarr, coordsys):
-    """Plot array configurations"""
-    tier = 'station'
+def plot_arrayconfiguration(stnid, bandarr, coordsys='local'):
+    """Plot different kinds of array configurations. """
     if stnid == 'ILT':
         tier = 'ILT'
         stnId_list = list_stations()
         pos = []
         names = []
-        for stnid in stnId_list:
-            names.append(stnid)
-            stnpos, stnrot, stnrelpos, stnintilepos = \
-                    getArrayBandParams(stnid, bandarr)
+        for stnid_ in stnId_list:
+            names.append(stnid_)
+            stnpos, stnrot, stnrelpos, stnintilepos = getArrayBandParams(stnid_, bandarr)
             pos.append(np.asarray(stnpos).squeeze().tolist())
         pos = np.array(pos)
     else:
         if bandarr == 'tile':
             tier = 'tile'
             bandarr = 'HBA'
-        stnpos, stnrot, stnrelpos, stnintilepos = getArrayBandParams(\
-                                                                 stnid, bandarr)
+        stnpos, stnrot, stnrelpos, stnintilepos = getArrayBandParams(stnid, bandarr)
         if coordsys == 'local':
             stnrelpos = stnrelpos * stnrot
             stnintilepos = stnintilepos * stnrot
@@ -216,25 +163,84 @@ def plot_arrayconfiguration(stnid, bandarr, coordsys):
             nameprefix = 'ant'
         names = [nameprefix+str(elem) for elem in range(pos.shape[0])]
     # Plot
+    from mpl_toolkits.mplot3d import Axes3D  # Needed for projection='3d' to work.
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot(pos[:,0],pos[:,1],pos[:,2],'*')
-    for idx, name in enumerate(names):
-        ax.text(pos[idx,0],pos[idx,1],pos[idx,2],name)
+    ax.set_title("Array configuration of {} {} in coordsys {}".format(stnid, bandarr,
+                                                                      coordsys))
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
     ax.set_aspect('equal')
+    ax.plot(pos[:, 0], pos[:, 1], pos[:, 2], '*')
+    for idx, name in enumerate(names):
+        ax.text(pos[idx,0],pos[idx,1],pos[idx,2], name, fontsize=7)
+    if stnid != 'ILT':
+        ax.set_zlim(1e5*ax.get_zlim3d())
     plt.show()
 
 
-def printmaxbaselines():
-    m= maxbaselinelen()
+def print_maxbaselines():
+    """print maximum baseline for all stations."""
+    m = max_stn_baselines()
     for k in m.keys():
         for b in m[k].keys():
-            print k, b, m[k][b]
+            print(k, b, m[k][b])
+
+
+def main():
+    """Export AntennaField data for all or selected stations to casa CSV files."""
+    stnId_list = list_stations()
+    stnId_list.sort(key=lambda stnid: stnid[2:])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--stationid',
+                        help="""Station ID to process.
+                        Choose from {}
+                        If not given, do all.
+                        """.format(stnId_list))
+    parser.add_argument('-b', '--bandarr',
+                        help="""Band array to process.
+                                Choose from {}.
+                                If not given, do all.
+                                """.format(BANDARRS))
+    args = parser.parse_args()
+
+    if args.stationid is not None:
+        stnId_list = [args.stationid]
+    bandarrs = BANDARRS
+    if args.bandarr is not None:
+        bandarrs = [args.bandarr]
+
+    nrstns = len(stnId_list)
+    the_maxbaselines = max_stn_baselines()
+    for bandarr in bandarrs:
+        header = _get_casacfg_header('ILT', bandarr)
+        #        if bandarr == 'LBA':
+        #            diam = 55.5
+        #        else:
+        #            diam = 63.3
+        outtable = np.zeros(nrstns, dtype=CASA_CFG_DTYPE)
+        for stnnr, stnid in enumerate(stnId_list):
+            if not (stnid == 'NenuFAR' and bandarr == 'HBA'):
+                print('Doing {} {}'.format(stnid, bandarr))
+                AntFld = parseAntennaField(stnid)
+                position = AntFld[bandarr]['POSITION']
+                outtable['X'][stnnr], outtable['Y'][stnnr], outtable['Z'][stnnr] = position
+                try:
+                    outtable['Diam'][stnnr] = the_maxbaselines[stnid][bandarr]
+                except KeyError:
+                    outtable['Diam'][stnnr] = the_maxbaselines[stnid]['HBA0']
+                outtable['Name'][stnnr] = stnid
+                output_arrcfg_station(stnid, bandarr)
+                output_rotmat_station(stnid, bandarr)
+            if bandarr == 'HBA' and stnid != 'NenuFAR':
+                output_arrcfg_tile(stnid)
+        # output array cfg_for ILT for this bandarr:
+        filename = os.path.join(CASA_CFG_DEST, "ILT_" + bandarr + '.cfg')
+        np.savetxt(filename, outtable, fmt=CASA_CFG_FMT, header=header)
 
 
 if __name__ == '__main__':
-    output_all_arrcfg_ILT()
-    output_all_stations_arrcfg()
-    output_all_tile_arrcfg()
-    output_all_rotmats()
+    main()
 
