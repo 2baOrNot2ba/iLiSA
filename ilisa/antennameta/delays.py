eeaa7728c7c6777e336711f4b4ab3c0aa4a7a2bb
Tobia D. Carozzi <tobia@chalmers.se>
2020-09-30 12:57:36 +0200

    Cleaned up delays.py.

--- ilisa/antennameta/delays.py
+++ ilisa/antennameta/delays.py
@@ -5,25 +5,26 @@
 import sys
 import os
 import numpy as np
-#import pkg_resources
-#my_data = pkg_resources.resource_filename(__name__, "share/StaticMetaData")
 
 
-STATICMETADATA = os.path.join(os.path.dirname(__file__),'share/StaticMetaData/')
+STATICMETADATA = os.path.join(os.path.dirname(__file__), 'share/StaticMetaData/')
 CABLEDELAYDIR = STATICMETADATA
-TABCOLUMNS = ('RCU','LBL_len','LBL_delay','LBH_len','LBH_delay','HBA_len','HBA_delay')
+TABCOLUMNS = ('RCU', 'LBL_len', 'LBL_delay', 'LBH_len', 'LBH_delay', 'HBA_len',
+              'HBA_delay')
 TABFORMATS = ('i4', 'f4',     'f4',       'f4',     'f4',       'f4',     'f4')
 
+
 def _load_stn_cabledelay_file(f):
-    lengthsNdelays = np.loadtxt(f, dtype={
+    lengths_n_delays = np.loadtxt(f, dtype={
                        'names': TABCOLUMNS,
                        'formats': TABFORMATS})
-    return lengthsNdelays
+    return lengths_n_delays
 
 
 def _stnid2filename(stnid):
     filename = stnid+'-'+'CableDelays.conf'
-    return os.path.join(CABLEDELAYDIR,filename)
+    return os.path.join(CABLEDELAYDIR, filename)
+
 
 def _get_units(quantitystr):
     arr, qtype = quantitystr.split('_')
@@ -32,24 +33,27 @@ def _get_units(quantitystr):
     elif qtype == 'delay':
         unit = 'ns'
     else:
-        raise ValueError, "Unknown quantity type"
+        raise ValueError("Unknown quantity type")
     return unit
 
 
 def get_stn_cabledelays(stnid):
     f = _stnid2filename(stnid)
-    lengthsNdelays = _load_stn_cabledelay_file(f)
-    return lengthsNdelays
+    lengths_n_delays = _load_stn_cabledelay_file(f)
+    return lengths_n_delays
 
 
-if __name__ == '__main__':
+def main():
     stnid = sys.argv[1]
     quantity = sys.argv[2]
     if quantity not in TABCOLUMNS:
-        raise ValueError, "Choose one of the following quantities: {}".format(TABCOLUMNS[1:])
+        raise ValueError("Choose one of the following quantities: {}".format(TABCOLUMNS[1:]))
     unit = _get_units(quantity)
-    lengthsNdelays = get_stn_cabledelays(stnid)
-    print("RCU [#] {} [{}]".format(quantity,unit))
-    for row, rcu in enumerate(lengthsNdelays['RCU']):
-        print("{}       {}".format(rcu, lengthsNdelays[quantity][row]))
+    lengths_n_delays = get_stn_cabledelays(stnid)
+    print("RCU [#] {} [{}]".format(quantity, unit))
+    for row, rcu in enumerate(lengths_n_delays['RCU']):
+        print("{}       {}".format(rcu, lengths_n_delays[quantity][row]))
 
+
+if __name__ == '__main__':
+    main()
