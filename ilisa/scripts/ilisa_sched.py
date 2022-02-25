ec011e3f18607ad305803840c852c6b5e24572d8
Tobia Carozzi <tobia@chalmers.se>
2022-02-25 14:23:52 +0100

    Added: make 'at' command start early in order to start ilisa_obs in time.

--- ilisa/scripts/ilisa_sched.py
+++ ilisa/scripts/ilisa_sched.py
@@ -5,32 +5,24 @@ import datetime
 import argparse
 import yaml
 
-now_timestamp = time.time()
-ut2lt_offset = datetime.datetime.fromtimestamp(now_timestamp) \
-               - datetime.datetime.utcfromtimestamp(now_timestamp)
-del now_timestamp
+__now_timestamp = time.time()
+ut2lt_offset = datetime.datetime.fromtimestamp(__now_timestamp) \
+               - datetime.datetime.utcfromtimestamp(__now_timestamp)
+del __now_timestamp
 DEFAULT_PROJ = 0
+# Margin of time before starttime 'at' command issued
+BEFORE_AT_MARGIN = datetime.timedelta(seconds=6)
 
 def sched2at(schedfile):
     with open(schedfile, 'rb') as file:
         schedtab = yaml.safe_load(file)
     # Setup possible global schedule variables
     mockflag = ''
-    try:
-        schedtab['mockrun']
-    except KeyError:
-        pass
-    else:
-        if schedtab['mockrun']:
-            mockflag = '-m'
-    try:
-        station = schedtab['station']
-    except KeyError:
-        station = None
-    try:
-        proj = schedtab['project']
-    except KeyError:
-        proj = DEFAULT_PROJ
+    mockrun = schedtab.get('mockrun', False)
+    if mockrun:
+        mockflag = '-m'
+    station = schedtab.get('station')
+    proj = schedtab.get('project', DEFAULT_PROJ)
 
     schedlines = schedtab['schedule']
     for schedline in schedlines:
@@ -38,19 +30,22 @@ def sched2at(schedfile):
         if type(schedline['start']) == datetime.datetime:
             start_ut = schedline['start']
         elif type(schedline['start']) == str:
-            start_ut = datetime.datetime.strptime(schedline['start'], '%Y-%m-%dT%H:%M:%S')
+            start_ut = datetime.datetime.strptime(schedline['start'],
+                                                  '%Y-%m-%dT%H:%M:%S')
         else:
             raise RuntimeError(type(schedline['start']))
-        start_lt =  start_ut + ut2lt_offset
+        startat_lt = (start_ut + ut2lt_offset - BEFORE_AT_MARGIN)
+        sleep_before_at_secs = 0
+        if startat_lt.second > 0:
+            sleep_before_at_secs = startat_lt.second
+            startat_lt = startat_lt.replace(second=0)
+
         # Setup `at` pipe
         atcmdv = ['at', '-t']
-        atcmdv.append(start_lt.strftime('%Y%m%d%H%M'))
-        p = subprocess.Popen(atcmdv, stdin=subprocess.PIPE)
+        atcmdv.append(startat_lt.strftime('%Y%m%d%H%M'))
         # Setup project:
-        try:
-            proj = schedline['project']
-        except KeyError:
-            pass
+        proj = schedline.get('project') if schedline.get('project') else proj
+        proj = str(proj)
         # Setup station:
         try:
             station = schedline['station']
@@ -58,19 +53,27 @@ def sched2at(schedfile):
             if not station:
                 raise RuntimeError('No station specified')
         # Setup commands for ilisa_cmd
-        schedline_cmd = schedline['cmd']
-        try:
-            scansesfile = schedline['session']
-        except KeyError:
-            scansesfile = None
-        if scansesfile:
-            cmd, arg = 'obs', scansesfile
+        cli_name = 'ilisa_cmd'  # Deprecated original monolithic ilisa command
+        cli_argv = ['-t', schedline['start'], '-s', station, mockflag]
+        if schedline['cmd'] == 'obs':
+            cli_name = 'ilisa_obs'
+            cli_argv += ['-p', proj, schedline.get('session')]
         else:
-            cmd, arg = 'adm', schedline_cmd
-        cmdline = 'ilisa_cmd -t {} -p {} -s {} {} {} {}'.format(schedline['start'],
-                    proj, station, mockflag, cmd, arg)
+            cli_name = 'ilisa_adm'
+            cli_argv += [schedline['cmd']]
+        cmdline = [cli_name] + cli_argv
+        cmdline = ' '.join(cmdline)
+
+        # Send shell commands to setup at call
+        p = subprocess.Popen(atcmdv, stdin=subprocess.PIPE)
+        # First send sleep before ilisa command (at only does down to minutes)
+        sleepcmd = ''
+        if sleep_before_at_secs:
+            sleepcmd = f"sleep {sleep_before_at_secs}\n"
+            #p.communicate(input=sleepcmd.encode())
         # Send ilisa_cmd to pipe
-        cmdline_wlog = '{} >> err_{}.log\n'.format(cmdline, station)
+        cmdline_wlog = '{} {} >> err_{}.log\n'.format(sleepcmd, cmdline,
+                                                      station)
         p.communicate(input=cmdline_wlog.encode())
 
 
