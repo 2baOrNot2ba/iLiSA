ffe05b24d41cfee54d42b61e832ade6b572e8a92
Tobia Carozzi <tobia@chalmers.se>
2022-03-02 11:29:35 +0100

    Changed: moved LATEST_SESS_DATAFILE to operations.__init__.py and renamed it LATESTDATAFILE

--- ilisa/operations/__init__.py
+++ ilisa/operations/__init__.py
@@ -5,6 +5,8 @@ USER_CONF_DIR = os.path.expanduser('~/.config/ilisa/')
 USER_DATA_DIR = os.path.expanduser('~/.local/share/ilisa/')
 USER_CACHE_DIR = os.path.expanduser('~/.cache/ilisa/')
 
+LATESTDATAFILE = os.path.join(USER_CACHE_DIR, "latestdatafiles.txt")
+
 def default_access_lclstn_conf():
     """Return the default access local station configuration."""
     accessconffile = os.path.join(USER_CONF_DIR, 'access_lclstn.conf')
