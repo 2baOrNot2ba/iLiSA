3307c3862e3d5beae4a8265a865f588f3d899214
Tobia Carozzi <tobia@chalmers.se>
2023-02-09 20:27:22 +0100

    Added: Beampattern handling functions & beamform_image() now uses FoV size in flux calc.

--- ilisa/calim/imaging.py
+++ ilisa/calim/imaging.py
@@ -4,6 +4,7 @@ Provides support direct imaging of LOFAR stand-alone data such as ACC and XST.
 import sys
 import os
 import argparse
+import pkg_resources
 
 import numpy
 import matplotlib.pyplot as plt
@@ -16,6 +17,7 @@ import ilisa.antennameta.antennafieldlib as antennafieldlib
 from ilisa.operations import data_io as data_io
 from ilisa.operations.directions import _req_calsrc_proc, pointing_tuple2str,\
                                           directionterm2tuple
+from ilisa.operations.modeparms import rcumode2sbfreqs, freq2sb, freq2bandarr
 from . import visibilities as vsb
 
 try:
@@ -37,6 +39,122 @@ def imggrid_res(ll, mm):
     dll = ll[0,1] - ll[0,0]
     dmm = mm[1,0] - mm[0,0]
     return dll, dmm
+
+
+def beamformed_pattern(stn2Dcoord, freq):
+    """
+    Plot beamformed beam flux pattern
+
+    Parameters
+    ----------
+    stn2Dcoord: array
+        Positions of array elements.
+    freq: float
+        Frequency.
+
+    Returns
+    -------
+    ll, mm: array
+        Direction cosine grids.
+    skyimages: tuple
+        Polarized sky images.
+    """
+    print("Computing beampattern for freq {}".format(freq))
+    nrants = stn2Dcoord.shape[0]
+    vis_xx = numpy.ones((nrants, nrants))
+    vis_yy = vis_xx
+    vis_xy = numpy.zeros_like(vis_xx)
+    vis_yx = vis_xy
+    vis_pol = numpy.array([[vis_xx, vis_xy],[vis_yx, vis_yy]])
+    skyimages, ll, mm \
+        = beamformed_image(vis_pol, stn2Dcoord.T, freq, nrpix=201)
+    return ll, mm, skyimages
+
+
+def beam_pat_shape(ll, mm, images_bf_pat):
+    """
+    Assess beamformed pattern
+
+    Parameters
+    ----------
+    ll, mm: array
+        Direction cosine grids.
+    images_bf_pat: tuple
+        Polarized images of beamformed pattern.
+
+    Returns
+    -------
+    major_diam: float
+        Major diameter of beam ellipse
+    minor_diam: float
+        Minor diameter of beam FWHM.
+    elltilt: float
+        Angle of tilt of beam ellipse in radians.
+    """
+    def maskbb(crdgrd, msk):
+        msk_crdgrd = msk * crdgrd
+        crd_max = numpy.amax(msk_crdgrd)
+        crd_min = numpy.amin(msk_crdgrd)
+        return (crd_max+crd_min)/2, crd_max-crd_min
+
+    image_0 = numpy.real(images_bf_pat[0])
+    beamcenter = numpy.unravel_index(numpy.argmax(image_0), image_0.shape)
+    fluxmax = image_0[beamcenter]
+    halfmax = fluxmax/2.0
+    beammask = numpy.zeros_like(image_0)
+    beammask[image_0>halfmax]=1.0
+    ll_me, mm_me = numpy.mean(beammask*ll), numpy.mean(beammask*mm)
+    ll_cntr, mm_cntr = ll-ll_me, mm-mm_me
+    ll2mm2 = numpy.sqrt(ll_cntr**2+mm_cntr**2)
+    semimajor_idx = numpy.unravel_index(numpy.argmax(beammask*ll2mm2),
+                                        image_0.shape)
+    semimajor_vec = numpy.array([ll_cntr[semimajor_idx], mm_cntr[semimajor_idx]]
+                                )
+    if semimajor_vec[0]<0.0:
+        semimajor_vec = -semimajor_vec
+    semiminor_uvec = numpy.array([-semimajor_vec[1], semimajor_vec[0]])/ \
+                     (numpy.linalg.norm(semimajor_vec))
+    semiminor_llmm = semiminor_uvec[0]*ll_cntr+semiminor_uvec[1]*mm_cntr
+    smin_cntr, smin_ext = maskbb(semiminor_llmm, beammask)
+    major_diam = 2*numpy.linalg.norm(semimajor_vec)
+    minor_diam = smin_ext
+    elltilt = numpy.arctan2(semimajor_vec[1], semimajor_vec[0])
+    return major_diam, minor_diam, elltilt
+
+
+def stn_beam_fov(stnid, antset, freq, _use_lookuptab=True):
+    """
+    Return station beam field-of-view size
+
+    stnid: str
+        Station ID.
+    antset: str
+        Band ID.
+    freq: float
+        Frequency.
+    _use_lookuptab : bool
+        Use lookup table instead of calculating FoV size.
+
+    Returns
+    -------
+    fov_size: float
+        FoV size in direction cosine.
+    """
+    if not _use_lookuptab:
+        stn_pos, stn_rot, stn_antpos, stn_intilepos \
+            = antennafieldlib.get_antset_params(stnid, antset)
+        antpos_uv = vsb.rot2uv(stn_antpos, stn_rot)
+        ll, mm, bfps = beamformed_pattern(antpos_uv, freq)
+        madi, midi, tlt = beam_pat_shape(ll, mm, bfps)
+    else:
+        bandarr = freq2bandarr(freq)
+        beamshape_path = pkg_resources.resource_filename(__name__,
+                            'beamshape_' + stnid + '_' + bandarr + '.npy')
+        beamshapes = numpy.load(beamshape_path)
+        freqidx = numpy.argmin(numpy.abs(beamshapes[:,0] - freq))
+        freq_cntr, madi, midi, tlt, fov_size = beamshapes[freqidx, :]
+    fov_sz_dlm = madi*midi/4*numpy.pi
+    return fov_sz_dlm
 
 
 def airydisk_radius(freq, d):
@@ -138,7 +256,8 @@ def phasedup_vis(vis, srcname, t, freq, polrep, stn_pos, stn_antpos):
 
 
 def beamformed_image(xstpol, stn2Dcoord, freq, use_autocorr=True,
-                     lmsize=2.0, nrpix=101, polrep='linear', fluxperbeam=True):
+                     lmsize=2.0, nrpix=101, polrep='linear', fluxperbeam=True,
+                     fov_sz=0.0):
     """
     Beamformed image XSTpol data.
 
@@ -165,6 +284,8 @@ def beamformed_image(xstpol, stn2Dcoord, freq, use_autocorr=True,
         (If dreamBeam package not accessible only 'linear' is possible)
     fluxperbeam : bool
         If True, then the flux is per beam, else the flux is per sterradian.
+    fov_sz: float
+        Field-of-view size.
 
     Returns
     -------
@@ -200,6 +321,8 @@ def beamformed_image(xstpol, stn2Dcoord, freq, use_autocorr=True,
                            + numpy.einsum('ij,k->ijk', mm, posV)))
     bfbf = numpy.einsum('ijk,ijl->ijkl', bf, numpy.conj(bf))
     nrm = nrbls
+    if fov_sz:
+        nrm *= fov_sz
     skyimag_xx = (numpy.einsum('ijkl,kl->ij', bfbf, xstpol[0, 0, ...].squeeze())
                   / nrm)
     skyimag_xy = (numpy.einsum('ijkl,kl->ij', bfbf, xstpol[0, 1, ...].squeeze())
@@ -271,7 +394,7 @@ def nearfield_grd_image(cvcobj, filestep, cubeslice, use_autocorr=False):
 
 
 def cvc_image(cvcobj, filestep, cubeslice, req_calsrc=None, pbcor=False,
-              fluxperbeam=True, autocorr=False, polrep='stokes'):
+              fluxperbeam=True, autocorr=False, polrep='stokes', fov_sz=0.0):
     """
     Image CVC object using beamformed synthesis
 
@@ -293,6 +416,8 @@ def cvc_image(cvcobj, filestep, cubeslice, req_calsrc=None, pbcor=False,
         Include autocorrelations
     polrep : str
         Polarization representation to use for image.
+    fov_sz: float
+        Size of field-of-view dict over frequencies.
 
     Returns
     -------
@@ -338,11 +463,11 @@ def cvc_image(cvcobj, filestep, cubeslice, req_calsrc=None, pbcor=False,
         fov = 2*airydisk_radius(freq, d)
         lmsize = 1.0*fov
 
-    # Make image on phased up visibilities
+        # Make image on phased up visibilities
     imgs_lin, ll, mm = beamformed_image(
         cvpu_lin, UVWxyz.T, freq, use_autocorr=autocorr, lmsize=lmsize,
         nrpix=43, #21, 43
-        polrep='linear', fluxperbeam=fluxperbeam)
+        polrep='linear', fluxperbeam=fluxperbeam, fov_sz=fov_sz)
 
     # Potentially apply primary beam correction 
     if pbcor and CANUSE_DREAMBEAM:
@@ -627,6 +752,11 @@ def image(dataff, filenr, sampnr, phaseref, correctpb, fluxpersterradian,
         if mockdata:
             modality = 'mock'
     stnid = cvcobj.scanrecinfo.get_stnid()
+    antset = cvcobj.scanrecinfo.get_bandarr()
+    freqs =  cvcobj.getfreqs()
+    fovs = {}
+    for freq in freqs:
+        fovs[freq] = stn_beam_fov(stnid, antset, freq)
     for fileidx in range(filenr, cvcobj.getnrfiles()):
         integration = cvcobj.scanrecinfo.get_integration()
         intgs = len(cvcobj.samptimeset[fileidx])
@@ -634,9 +764,9 @@ def image(dataff, filenr, sampnr, phaseref, correctpb, fluxpersterradian,
             t = cvcobj.samptimeset[fileidx][tidx]
             freq = cvcobj.freqset[fileidx][tidx]
             skyimages, ll, mm, _phaseref_ = \
-                cvc_image(cvcobj, fileidx, tidx, phaseref,
-                                  polrep=polrep, pbcor=correctpb,
-                                  fluxperbeam=fluxperbeam, autocorr=autocorr)
+                cvc_image(cvcobj, fileidx, tidx, phaseref, polrep=polrep,
+                          pbcor=correctpb, fluxperbeam=fluxperbeam,
+                          autocorr=autocorr, fov_sz=fovs[freq])
             plotskyimage(ll, mm, skyimages, polrep, t, freq, stnid, integration,
                          _phaseref_, modality, pbcor=correctpb, maskhrz=False,
                          fluxperbeam=fluxperbeam, plot_title='Imaged Sky')
@@ -704,5 +834,54 @@ def main_cli():
               args.correctpb, args.fluxpersterradian, args.autocorr)
 
 
+def beampat_cli():
+    """
+    Beampattern CLI
+
+    Plots or prints out the synthesized beam of a specified station, band-array,
+    and frequency.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--plot', action="store_true",
+                        help="Correct for primary beam")
+    parser.add_argument('-s' , '--stnid', type=str, default='')
+    parser.add_argument('-a', '--antset', type=str, default='')
+    parser.add_argument('-f', '--freq', type=float, default=0.0)
+    args = parser.parse_args()
+    if not args.stnid:
+        raise(RuntimeError, 'Choose stnid')
+    if not args.antset:
+        args.antset = 'LBA'  # TODO: HBA
+    if not args.freq:
+        if 'LBA' in args.antset:
+            rcumode = 3
+        freqs = rcumode2sbfreqs(rcumode)
+    else:
+        freqs = [args.freq]
+    beamshapes = []
+    print('Freq Major Minor Tilt FoV\n')
+    for freq in freqs:
+        stn_pos, stn_rot, stn_antpos, stn_intilepos \
+            = antennafieldlib.get_antset_params(args.stnid, args.antset)
+        antpos_uv = vsb.rot2uv(stn_antpos, stn_rot)
+        ll, mm, bfps = beamformed_pattern(antpos_uv, freq)
+        if args.plot:
+            plotskyimage(ll, mm, bfps, 'linear', 0, freq, args.stnid, 0)
+            plt.show()
+        madi, midi, tlt = beam_pat_shape(ll, mm, bfps)
+        beamshape = (freq, madi, midi, numpy.rad2deg(tlt), madi*midi/4*numpy.pi)
+        print(beamshape)
+        beamshapes.append(beamshape)
+    if len(freqs) > 1:
+        outfilename = 'beamshape_' + args.stnid + '_LBA.npy'
+        numpy.save(outfilename, beamshapes)
+
+
 if __name__ == "__main__":
-    main_cli()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd', type=str)
+    cmd = sys.argv.pop(1)
+    if cmd == 'main':
+        main_cli()
+    elif cmd == 'beampat':
+        beampat_cli()
