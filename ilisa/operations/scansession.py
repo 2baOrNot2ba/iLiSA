9c8c3897e6e42f58cd06ff06c47ca91e3fdae497
Tobia Carozzi <tobia@chalmers.se>
2024-07-02 17:47:46 +0200

    Changed: CLI `note` arg more clearly printed out

--- ilisa/operations/scansession.py
+++ ilisa/operations/scansession.py
@@ -711,7 +711,8 @@ def main_cli():
         args.time, args.project, args.mockrun, args.station, args.check,
         args.postprocess, args.file)
     )
-    _LOGGER.info('Session note: {}'.format(args.note))
+    if args.note:
+        _LOGGER.info('CLI session note: {}'.format(args.note))
     sesmeta_in = {}
     sesmeta_in['cli_start'] = args.time
     sesmeta_in['mockrun'] = args.mockrun
