e5defe79ea452e43b6187ebb296969a5e99fa930
Tobia Carozzi <tobia@chalmers.se>
2024-11-05 09:45:34 +0100

    Changed: transition to modern setup.

--- setup.py
+++ setup.py
@@ -1,48 +1,9 @@
-from setuptools import setup, find_packages
-import ilisa
+from setuptools import setup
 
-setup(name='iLiSA',
-      version=ilisa.__version__,
-      description="""Package for handling international LOFAR station in
-                  stand-alone mode""",
-      author='Tobia D. Carozzi',
-      author_email='tobia.carozzi@chalmers.se',
-      packages=find_packages(),
-      package_data={
-            'ilisa.operations': ['config/access_lclstn_DIST.conf',
-                                      'config/project_0_DIST.conf',
-                                      'scansess_examples/*.yaml'],
-            'ilisa.antennameta': ['share/CalTables/*/data/*.dat',
-                                  'share/StaticMetaData/*.conf']},
-      license='ISC',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: ISC License',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Mathematics',
-          'Topic :: Scientific/Engineering :: Visualization'
-      ],
-      install_requires=[
-          'numpy',
-          'scipy',
-          'python-casacore',
-          'matplotlib',
-          'PyYAML',
-          'h5py'
-      ],
-      entry_points={
-          'console_scripts': [
-              'ilisa_adm = ilisa.operations.stationdriver:main_cli',
-              'ilisa_rec = ilisa.operations.scan:main_cli',
-              'ilisa_obs = ilisa.operations.scansession:main_cli',
-              'ilisa_view = ilisa.operations.data_io:main',
-              'ilisa_cal = ilisa.calim.calibration:main_cli',
-              'ilisa_image = ilisa.calim.imaging:main_cli',
-              'ilisa_sched = ilisa.scripts.ilisa_sched:main',
-              'pl_bfs = ilisa.pipelines.bfs_data:main_cli',
-              'pl_rec = ilisa.pipelines.bfbackend:bfsrec_main_cli'
-          ]
-      }
-      )
+# Next 3 lines might be needed due to bug https://github.com/pypa/pip/issues/7953 in setuptools <62.0
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+if __name__ == "__main__":
+    setup()
