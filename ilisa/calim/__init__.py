f48b751a0debd48d17d5e4bb1e102b2a04ece718
Tobia Carozzi <tobia@chalmers.se>
2025-01-31 10:25:36 +0100

    Changed: `calim` package bumped to v1.0

--- ilisa/calim/__init__.py
+++ ilisa/calim/__init__.py
@@ -4,5 +4,6 @@ Provides functions for calibration & imaging in radio interferometry
 import os
 from scipy.constants import speed_of_light as SPEED_OF_LIGHT
 
+__version__ = '1.0'
 
 USER_DATA_DIR = os.path.expanduser('~/.local/share/ilisa/calim')
