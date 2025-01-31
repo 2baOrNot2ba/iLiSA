691f7f36f05de8800032cce46c54fef1f588f0c1
Tobia Carozzi <tobia@chalmers.se>
2025-01-31 10:00:04 +0100

    Changed: Get USER_DATA_DIR in calim __init__

--- ilisa/calim/__init__.py
+++ ilisa/calim/__init__.py
@@ -1,5 +1,8 @@
 """
 Provides functions for calibration & imaging in radio interferometry
 """
-__version__ = '0.1'
+import os
 from scipy.constants import speed_of_light as SPEED_OF_LIGHT
+
+
+USER_DATA_DIR = os.path.expanduser('~/.local/share/ilisa/calim')
