6ef063420a54245cfdf3cbf2fa58ac3e8e162c9a
Tobia Carozzi <tobia@chalmers.se>
2025-09-02 23:04:05 +0200

    Changed: skymodel_visibility() explicitly uses horizontaldipoles_jones()

--- ilisa/calim/skymodels.py
+++ ilisa/calim/skymodels.py
@@ -28,7 +28,7 @@ from .imaging import plotskyimage, imggrid_res, fiducial_image,\
     integrate_lm_image
 from .visibilities import cov_polidx2flat, calc_uvw, rot2uv,\
     point_source_vis2d, layout_abs2rel
-from .beam import dualdipole_cov_patt
+from .beam import horizontaldipoles_jones, jones2cov_patt
 from .im_process import n_from_lm
 
 
@@ -535,7 +535,8 @@ def skymodel_visibility(t, stn_pos, freq, stn_antpos, gs_model, ant_model=''):
                 phaseref_ccm['refer'])
     uvw_sl = calc_uvw(t, phaseref, stn_pos, stn_antpos)
     if ant_model == 'dual_dipole45':
-        (cov_xx, cov_xy, cov_yx, cov_yy) = dualdipole_cov_patt(ll, mm)
+        jones_patt = horizontaldipoles_jones(ll, mm, rotzen=np.pi/2, reff=0.)
+        (cov_xx, cov_xy, cov_yx, cov_yy) = jones2cov_patt(jones_patt)
         imag_xx = cov_xx * img_S0 / 2.0
         imag_xy = cov_xy * img_S0 / 2.0
         imag_yx = cov_yx * img_S0 / 2.0
