16117826a7f7a60db8849f343c341914a72b9c4d
Tobia Carozzi <tobia@chalmers.se>
2026-03-11 11:27:17 +0100

    Added: MAX_NR_BEAMLETS_PER_LANE

--- ilisa/operations/modeparms.py
+++ ilisa/operations/modeparms.py
@@ -19,6 +19,7 @@ nrofrcus = 192  # Number of RCUs
 MIN_STATS_INTG = 1.0  # Minimum integration for statistics data in seconds.
 MAX_NRLANES = 4
 BASE_NR_BEAMLETS = 244  # Base number of total beamlets available
+MAX_NR_BEAMLETS_PER_LANE = BASE_NR_BEAMLETS // MAX_NRLANES  # 61
 NRBEAMLETSBYBITS = {16:   BASE_NR_BEAMLETS,
                     8:  2*BASE_NR_BEAMLETS,
                     4:  4*BASE_NR_BEAMLETS}
